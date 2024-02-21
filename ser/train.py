a8e4a9efb09f7ddef814b6fd2199a1c925639617
OwenPullen <owenpullen@hotmail.co.uk>
2024-02-21 11:19:15 +0000

    logging + modularisation

--- ser/train.py
+++ ser/train.py
@@ -1,10 +1,14 @@
 import torch
 # from ser.data import training_dataloader, validation_dataloader
 import torch.nn.functional as F
+from ser.logging import log_training, log_validation
+from pathlib import Path
 
-def train(model, device, optimizer, epochs, training_dataloader, validation_dataloader):
+PROJECT_ROOT = Path(__file__).parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+
+def train(model, device, optimizer, epoch, training_dataloader, logging_dir):
     # train
-    for epoch in range(epochs):
         for i, (images, labels) in enumerate(training_dataloader):
             images, labels = images.to(device), labels.to(device)
             model.train()
@@ -13,10 +17,16 @@ def train(model, device, optimizer, epochs, training_dataloader, validation_data
             loss = F.nll_loss(output, labels)
             loss.backward()
             optimizer.step()
+            batch = f"{i}/{len(training_dataloader)}"
             print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
+                f"Train Epoch: {epoch} | Batch: {batch}"
                 f"| Loss: {loss.item():.4f}"
             )
+            log_training(epoch, batch, loss.item(), logging_dir)
+        
+        
+
+def validate(model, device, validation_dataloader, epoch, logging_dir):
         # validate
         val_loss = 0
         correct = 0
@@ -34,3 +44,16 @@ def train(model, device, optimizer, epochs, training_dataloader, validation_data
             print(
                 f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
                 )
+            log_validation(epoch, val_loss, val_acc, logging_dir)
+
+            
+            
+def training_wrapper(model, device, optimizer,
+                      epochs, training_dataloader,
+                        validation_dataloader, logging_dir):
+    for epoch in range(epochs):
+          train(model, device, optimizer, epoch, training_dataloader, logging_dir)         
+          validate(model, device, validation_dataloader, epoch, logging_dir)
+    
+    torch.save(model.state_dict(), PROJECT_ROOT / "experiments" / logging_dir / "trained_model.pth")
+          
