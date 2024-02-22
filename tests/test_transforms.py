f632a2b4b5a32d66c9e7130d1c6e74ac2a2d723e
OwenPullen <owenpullen@hotmail.co.uk>
2024-02-22 17:04:27 +0000

    unit tests for flip and normalize

---
+++ tests/test_transforms.py
@@ -0,0 +1,26 @@
+import torch
+from torchvision import transforms
+from ser.transforms import flip, normalize
+from numpy import mean, std
+
+def test_flip():
+    x = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    x = torch.tensor(x)
+    flipped_tensor = flip()(x)
+    assert torch.all(flipped_tensor == torch.tensor(
+        [[9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1]])
+    ) == True
+
+def test_normalize():
+    x = [[[1, 2, 3], [1, 2, 3], [1, 2, 3]]]
+    x = torch.tensor(x, dtype=torch.float)
+    normalized_tensor = normalize()(x)
+    normalized_tensor == torch.tensor([[[1, 3, 5],
+                                        [1, 3, 5],
+                                        [1, 3, 5]]],
+                                        dtype=torch.float)
+    assert True
+    
+
